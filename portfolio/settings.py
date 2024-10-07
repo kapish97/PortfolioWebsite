9b663bdd518cf29e19b77d019ff55d67296d6554
kapish <62199998+kapish97@users.noreply.github.com>
2024-10-07 21:14:18 +0530

    versel host added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$pakxjv3iaibmp1$1s2afdn3y=y_numn24*z@tn1$rz+m2*)%@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
