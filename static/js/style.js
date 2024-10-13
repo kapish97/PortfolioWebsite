d43ca9f98fe94fb6366e45c295cdbe6adafa3d03
kapish <62199998+kapish97@users.noreply.github.com>
2024-10-13 18:42:27 +0530

    collect static run

--- static/js/style.js
+++ static/js/style.js
@@ -20,4 +20,21 @@ $(document).ready(function(){
         typeSpeed: 100,
         loop: true
       });
+
+    //owl carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout: 2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            900:{
+                items:2
+            }
+        }
+    })
 });
